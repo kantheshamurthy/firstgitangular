078343edf3732b607741515b8d3ce2933257c09b
Kanthesha S B. Murthy <kanthesha@cce.soft.com>
2018-12-17 19:05:46 +0530

    testing first time adding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular7app';
+  title = 'Kanthesha Murthy S B';
 }
---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.scss']
+})
+export class RecipeListComponent implements OnInit {
+  recipes:Recipe[] = [
+    new Recipe('A test recipe', 'A test Desc', 'https://img.taste.com.au/ITgbQUXM/w643-h428-cfill-q90/taste/2016/11/butter-chicken-with-naan-81484-1.jpeg'),
+    new Recipe('A test recipe2', 'A test Desc2', 'https://img.taste.com.au/ITgbQUXM/w643-h428-cfill-q90/taste/2016/11/butter-chicken-with-naan-81484-1.jpeg'),
+    new Recipe('A test recipe3', 'A test Desc3', 'https://img.taste.com.au/ITgbQUXM/w643-h428-cfill-q90/taste/2016/11/butter-chicken-with-naan-81484-1.jpeg')
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
